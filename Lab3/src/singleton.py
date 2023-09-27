83a699e8c5911cc855eab4db5122a48c25c41b86
Yingh Wang <wangyinghan1001@gmail.com>
2023-09-28 06:24:51 +0800

    add code for Lab3

--- Lab3/src/singleton.py
+++ Lab3/src/singleton.py
@@ -1,18 +1,26 @@
 class Logger:
-    _instance = None  # Private class variable to hold the single instance
+    _instance = None
 
     def __init__(self):
-        self.messages = []
+        raise RuntimeError("Call instance() instead of direct instantiation")
+    
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = super(Logger, cls).__new__(cls)
+            cls._instance.messages = []
+            print("Logger created exactly once")
+        else:
+            print("logger already created")
+        return cls._instance
 
     def add_message(self, message):
         self.messages.append(message)
 
 
 def main():
-    # Logger should only be initialized one time if it is properly
-    # refactored as a singleton class
     for i in range(3):
-        logger = Logger()
+        logger = Logger.instance()
         logger.add_message(f"Adding message number: {i}")
 
 
